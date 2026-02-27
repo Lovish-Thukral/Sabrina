72009eaef1bc07b46994911c6dabcd3feb8e66f6
lovish <Lovish>
2026-02-27 15:58:05 +0530

    Added TTS Module, Next to add it in main workflow

--- CoreTTS/main.py
+++ CoreTTS/main.py
@@ -6,55 +6,94 @@ import queue
 import threading
 
 class TTSModel:
-
+    """Persistent NeuTTS text-to-speech engine with streaming playback."""
     def __init__(
         self,
         modelPath="models/neutts/neutss-air-BF16.gguf",
         decoderPath="neuphonic/neucodec-onnx-decoder-int8"
     ):
-
+        self.modelPath = modelPath
+        self.decoderPath = decoderPath
+        self.model = None
+        self.running = False
+        with open("CoreTTS/Samples/codec.json", "r") as f:
+            self.voiceData = json.load(f)
+    
+    def start(self):
+        "Loads The Model To Memory, Must Be Called before play()"
+        self.running = True
         self.model = NeuTTS(
-            backbone_device="cpu",
-            backbone_repo=modelPath,
+            backbone_device="gpu",
+            backbone_repo=self.modelPath,
             language="en-us",
-            codec_repo=decoderPath,
+            codec_repo=self.decoderPath,
             codec_device="cpu"
         )
+        return
+    
+    def stop(self):
+        """Unloads the model."""
         self.running = False
+        self.model.backbone.close()
+        self.model = None
+        return
 
     def play(self, text:str, cloningchar = "Lily"):
+        """Generate and play speech from text.
+
+        Parameters
+        ----------
+        text : str
+            Input text to synthesize.
+
+        cloningchar : str
+            Voice profile name from codec.json.
         """
-        main Loop To Run the model
-        """
+        cloningCodec = numpy.array(self.voiceData[cloningchar]["Audio"], dtype=numpy.int32)
+        cloningScript = self.voiceData[cloningchar]["Script"]
+        streamQueue = queue.Queue()
         player = threading.Event()
-        with open("CoreTTS/Samples/codec.json", "r") as f:
-            voiceData = json.load(f)
-        cloningCodec = numpy.array(voiceData[cloningchar]["Audio"], dtype=numpy.int32)
-        cloningScript = voiceData[cloningchar]["Script"]
         
-        while self.running:
+        def generate_audio():
             chunks = self.model._infer_stream_ggml(input_text= text, ref_codes=cloningCodec, ref_text=cloningScript)
             chunksSize = 0
-            stream = sounddevice.OutputStream(
-                samplerate=23000,
-                channels=1,
-                dtype="float32",
-                blocksize=256
-            )
-            streamQueue = queue.Queue()
-            stream.start()
             for chunk in chunks:
                streamQueue.put(chunk)
                chunksSize += 1
                if chunksSize == 3:
                    player.set()
             streamQueue.put(None)
-            
+            if chunksSize < 3:
+                player.set()
+        
+        def stream_audio():
+            player.wait()
+            stream = sounddevice.OutputStream(
+                samplerate=23000,
+                channels=1,
+                dtype="float32"
+            )
+            stream.start()
+            while True:
+                chunk = streamQueue.get()
+                if chunk is None:
+                    break
+                stream.write(chunk)
+
+            stream.stop()
+            stream.close()
+        generate = threading.Thread(target=generate_audio)
+        consumer = threading.Thread(target=stream_audio)
+        generate.start()
+        consumer.start()
+        generate.join()
+        consumer.join()
 
     
 if __name__ == "__main__":
     tts = TTSModel()
-    tts.running = True
-    tts.play(text="Wait… did you hear that? No, listen carefully. It’s fine, don’t worry. Things are under control now. Stay alert, and let’s continue")
-    tts.running = False
-    tts.model.backbone.close()
+    tts.start()
+    for i in range(1, 10):
+        x = input()
+        tts.play(x)
+    tts.stop()
